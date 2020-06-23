28b0f1def7b22e654e760b21424ffc5e2b3a4239
Oliver Cook <olliewatford@gmail.com>
2020-06-23 22:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function add(){}
-function subtract(){}
-function multiply(){}
-function divide(){}
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(){
+  return a/b;
+}
