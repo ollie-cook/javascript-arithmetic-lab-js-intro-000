ec61e31cf13be0f49f2c0524ebaefc928d89bf6f
Oliver Cook <olliewatford@gmail.com>
2020-06-23 22:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a,b){
 function divide(a,b){
   return a/b;
 }
-function increment(n){ 
+function increment(n){
 return n++ ;
-} 
-function decrement(n){ 
+}
+function decrement(n){
 return n-- ;
 }
